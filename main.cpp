010b79ef54bb5d79826cc72006d070574498ed31
Hadeer Abdelnasser <101938448+hadeerabdelnaser@users.noreply.github.com>
2022-11-02 18:41:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ int main() {
     while (cin >> n1 >> n2) {
         BigReal x(n1), y(n2);
         BigReal z(x + y);
+        z.signReal();
         z.print();
+        cout << x.size() << endl;
+        cout << y.size() << endl;
+        cout << z.size() << endl;
         cout << endl;
     }
 }
